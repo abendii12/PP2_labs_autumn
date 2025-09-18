6a451f2bf9e988c92a69aea1783d8e9aff52503c
Dilnaz <d_aben@kbtu.kz>
2025-09-18 18:40:51 +0500

    second commit

---
+++ lab1/variables/names.py
@@ -0,0 +1,13 @@
+myvar = "John"
+my_var = "John"
+my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+myVariableName = "John"
+
+MyVariableName = "John"
+
+my_variable_name = "John"
+
