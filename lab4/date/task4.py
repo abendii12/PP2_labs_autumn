f16cc776a6db515fed9c843f9358aa25fcdeec70
Dilnaz <d_aben@kbtu.kz>
2025-10-09 22:59:50 +0500

    Update task4.py

--- lab4/date/task4.py
+++ lab4/date/task4.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-first_date = datetime(2024, 2, 14, 14, 30, 45)
-second_date = datetime(2024, 2, 16, 23, 45, 59)
+first_date = datetime(2025, 10, 9, 10, 59, 45)
+second_date = datetime(2025, 10, 10, 23, 59, 59)
 sub = second_date - first_date
 diff = sub.total_seconds()
 print("result: ", diff)
