a6e860e3854f0cc1a82f1e4856bb7c6ebc236995
Dilnaz <d_aben@kbtu.kz>
2025-10-23 21:20:45 +0500

    lab6

--- lab6/builtin/task1.py
+++ lab6/builtin/task1.py
@@ -1,3 +1,8 @@
 numbers = [2, 3, 4, 5]
 result = eval('*'.join(map(str, numbers)))  
 print("Product:", result)
+#2
+import math
+numbers = [2, 3, 4, 5]
+result = math.prod(numbers)
+print("Product:", result)
